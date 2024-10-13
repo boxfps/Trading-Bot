a86a4e5ef0d8694fd4e8edea13cfeb4651e2ae86
b0x <aidan625@outlook.com>
2024-10-13 14:43:47 -0400

    Fleshed out strategy & begun stock_data.py file (barely)

--- code/stock_data.py
+++ code/stock_data.py
@@ -1,1 +1,3 @@
 # This file will collect and handle stock data based on the provided list
+from stock_list import tsx_stocks # Import the predefined stock list
+import yfinance as yf   # Import the yahoo finance API in order to get stock info
