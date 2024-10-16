a9ad12a37043b56863cff0d07db2d38f65fd9482
b0x <aidan625@outlook.com>
2024-10-16 09:10:54 -0400

    Very small addition to probably fetch Stock Data from yf

--- README.md
+++ README.md
@@ -10,9 +10,3 @@ My (probably poor) attempt at creating a trading bot. Primarily in python. I wil
 My goal in doing this project is to create a automatic trading bot, that can consistently return 5% on a monthly basis. I will not be options trading (yet) and will purely be buying and selling stocks. 
 
 To understand more about my strategy, please see Strategy.txt
-
-
-# October 12th 2024
-Today I have simply organized my files, and grabbed a random list of stocks to trade. I will most likely be updating these in the future. 
-
-I have also begun to define my trading strategy, based on many many hours of research done before beginning this project. 
--- code/stock_data.py
+++ code/stock_data.py
@@ -1,3 +1,21 @@
 # This file will collect and handle stock data based on the provided list
+
 from stock_list import tsx_stocks # Import the predefined stock list
 import yfinance as yf   # Import the yahoo finance API in order to get stock info
+
+def get_stock_data(ticker, period='1y'):
+    # Fetch stock data for each ticker
+    stock = yf.Ticker(ticker)
+    return stock.history(period = period)
+
+def get_all_stock_data(stock_list, period = '1y'):
+    stock_data = {}
+    for stock in stock_list:
+        try:
+            print (f"Fetching data for {stock}...")
+            data = get_stock_data(stock, period)
+            stock_data[stock] = data
+        except Exception as e:
+            print (f"Error fetching data for {stock}: {e}")
+
+    return stock_data
